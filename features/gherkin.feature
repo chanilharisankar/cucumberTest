a07549e83b6e6068a88649aec69af09298b0ab7a
haris <haris@in-haris.local>
2017-12-29 16:41:21 +0530

    datatable

--- features/gherkin.feature
+++ features/gherkin.feature
@@ -1,13 +1,10 @@
 Feature: App login feature without param
+
   Scenario: successfully login to application with valid credentials
     Given application is open
-    When user loges in with username and password
+    When user loges in with Username and Password
+      | Username | Password   |
+      | Hari     | myPassword |
     Then application loges in
     And user closes the broswer
 
-Feature: App login feature with param
-  Scenario: successfully login to application with valid credentials
-    Given application is open with user "test1" and password "password"
-    When user loges in with username and password
-    Then application loges in
-    And user closes the broswer
--- src/main/java/stepDefinations/StepDefination.java
+++ src/main/java/stepDefinations/StepDefination.java
@@ -1,33 +1,37 @@
 package stepDefinations;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.List;
+
 public class StepDefination  {
 
-@Given("^application is open$")
-public void application_in_open() throws Throwable {
-System.out.println("application is open");
+    @Given("^application is open$")
+    public void application_in_open() throws Throwable {
+    System.out.println("application is open");
 
-}
+    }
 
-@When("^user loges in with username and password$")
-public void user_loges_in_with_username_and_password() throws Throwable{
-System.out.println("user_loges_in_with_username_and_password");
-}
+    @When("^user loges in with Username and Password$")
+    public void user_Loges_In_With_Username_And_Password(DataTable dataTable) throws Throwable {
+        List<List<String>> data =dataTable.raw();
+        System.out.println(data.get(1).get(0));
+    }
 
-@Then("^application loges in$")
-public void application_loges_in()throws Throwable{
-    System.out.println("application_loges_in");
-}
+    @Then("^application loges in$")
+    public void application_loges_in()throws Throwable{
+        System.out.println("application_loges_in");
+    }
 
-@And("^user closes the broswer$")
-public void user_closes_the_broswer$() throws Throwable{
-    System.out.println("user_closes_the_broswer$");
+    @And("^user closes the broswer$")
+    public void user_closes_the_broswer$() throws Throwable{
+        System.out.println("user_closes_the_broswer$");
 
-}
+    }
 
 
 
